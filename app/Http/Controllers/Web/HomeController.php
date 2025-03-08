075187ce53b239c2a68ec08521ea3f3ca58bc0d3
ismail <himiismail123@gmail.com>
2025-03-08 10:45:20 +0300

    Update HomeController.php

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -59,7 +59,7 @@ class HomeController extends Controller
 
     public function default_theme(): View|null
     {
-        $categories = CategoryManager::getCategoriesWithCountingAndPriorityWiseSorting();
+        $categories = CategoryManager::getCategoriesWithCountingAndPriorityWiseSorting(50);
 
         
         $userId = Auth::guard('customer')->user() ? Auth::guard('customer')->id() : 0;
