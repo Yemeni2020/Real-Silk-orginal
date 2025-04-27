40f4634f44503ab1f5b598d29e5db53c284aec6c
ismail <himiismail123@gmail.com>
2025-04-27 08:21:18 +0300

    تم رفع ترجمة الخاص بالمنشورات
    تم رفع ترجمة الخاص بالمنشورات

--- resources/themes/default/web-views/posts/posts-card.blade.php
+++ resources/themes/default/web-views/posts/posts-card.blade.php
@@ -3,7 +3,7 @@
             <a href="{{ route('posts.show', $post->slug) }}" class="list-group-item list-group-item-action mb-3 shadow-sm">
                 <div class="d-flex align-items-center">
                     @if($post->thumbnail)
-                        <img src="{{ getStorageImages(path: $post->thumbnail_full_url, type: 'product') }}" class="img-thumbnail me-3" style="width: 100px; height: 100px;" alt="{{ $post->title }}">
+                        <img src="{{ getStorageImages(path: $post->thumbnail_full_url, type: 'product') }}" class="img-thumbnail me-3" style="width: 200px; height:200px;" alt="{{ $post->title }}">
                     @endif
                     <div>
                         <h5 class="mb-1">{{ $post->title }}</h5>
